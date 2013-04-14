f1d5b488dd30dbd335f392ebb6e2e678f1cee033
marciusbezerra <marciusbezerra@gmail.com>
2013-04-14 14:56:31 -0300

    Fazendo um teste de commit...

--- BotIRC/Program.cs
+++ BotIRC/Program.cs
@@ -14,7 +14,7 @@ namespace BotIRC
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault( false );
             Application.Run(new Form1());
         }
     }
